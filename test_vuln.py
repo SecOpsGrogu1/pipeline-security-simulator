f716e6f34472313291bb1ada057edadd8ce72881
Paul M <156852405+SecOpsGrogu1@users.noreply.github.com>
2025-05-27 17:57:24 -0500

    test: add demo vulnerability to trigger pipeline failure

--- test_vuln.py
+++ test_vuln.py
@@ -1,4 +1,4 @@
 # This file intentionally contains vulnerabilities for scanner testing
-#SECRET_KEY = "mysecret"
+SECRET_KEY = "mysecret"  # Uncomment this line to demo a pipeline failure (medium severity)
 #result = eval("2+2")
 #hash = md5(b"data").hexdigest()
