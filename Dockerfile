9c3af00eae4a01ac80ba820be4891a99aca16717
Paul M <156852405+SecOpsGrogu1@users.noreply.github.com>
2025-05-28 10:12:25 -0500

    chore: delete all files from repo

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-# Secure Dockerfile for passing pipeline (same as Dockerfile.secure)
-FROM alpine:3.18
-RUN apk add --no-cache openssl
-CMD ["openssl", "version"]
--- test_vuln.py
+++
@@ -1,4 +0,0 @@
-# This file intentionally contains vulnerabilities for scanner testing
-SECRET_KEY = "mysecret"  # Uncomment this line to demo a pipeline failure (medium severity)
-#result = eval("2+2")
-#hash = md5(b"data").hexdigest()
